3a182113c744f642bb5e8a97bbafe36b4b071823
Prasad <kadimi.prasad@accenture.com>
2019-05-16 11:08:50 +0530

    sql answers

---
+++ root_power.py
@@ -0,0 +1,18 @@
+import math
+
+
+def find_result(integer):
+    result_list = []
+    for pwr in range(1, 625):
+        base_root_value = integer ** (1.0 / pwr)
+        if int(math.ceil(base_root_value)) ** pwr == integer:
+            pair_tuple = tuple([int(math.ceil(base_root_value)), pwr])
+            result_list.append(pair_tuple)
+    return result_list
+
+
+try:
+    integer = int(input('Enter a integer:'))
+    print(find_result(integer))
+except:
+    print('Invalid input. Try again')
